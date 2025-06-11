6251c22b332c05d003121911ac2904bd5a8fa29c
itsnavneetmishra <mishranavneeet900@gmail.com>
2025-06-11 11:06:05 +0530

    class

--- Day 2/program 1.py
+++ Day 2/program 1.py
@@ -1,11 +1,11 @@
-class Person():
-  def _init_(self,name,age,gender):
+class Person:
+  def __init__(self,name,age,gender):
     self.name=name
     self.age=age
     self.gender=gender
 
   def show_details(self):
-    print("The name is {}, age is {} and gender is {} ".format(name,age,gender))
+    print("The name is {}, age is {} and gender is {} ".format(self.name,self.age,self.gender))
 
 
 p1=Person("Navneet",18,"Male")
